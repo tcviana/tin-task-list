05eebec231e6df960c9a5e83125725993f8e9831
Tiago Viana <tcviana.sp@gmail.com>
2020-12-05 16:42:30 -0300

    Criação do projeto

---
+++ src/main/java/com/tinTaskList/tinTaskList/TinTaskListApplication.java
@@ -0,0 +1,13 @@
+package com.tinTaskList.tinTaskList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinTaskListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinTaskListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinTaskList/tinTaskList/TinTaskListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinTaskList.tinTaskList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinTaskListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
